87def2a05669fa6f08bea97811fcf06169239e5e
Fred Kranjec <frederick.kranjec-larose@hotmail.com>
2026-05-29 00:43:10 -0400

    Project Layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Portfolio from './pages/Portfolio'
+import Links from './pages/Links'
+import Contact from './pages/Contact'
 
-function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>FK — Portfolio</div>} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/"          element={<Home />} />
+        <Route path="/about"     element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/links"     element={<Links />} />
+        <Route path="/contact"   element={<Contact />} />
+      </Routes>
+    </Layout>
   )
 }
-
-export default App
